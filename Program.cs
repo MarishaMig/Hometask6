2f7ed31e9961db316daf0eb155461ebdb46f62c6
Marina Migurskaya <marina.migurskaya@gmail.com>
2023-12-17 16:03:49 +0300

    Task2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива. 
 a b c => “abcdef” 
 d e f */
-
+/*
 char[,] array = new char[4,3];
 string s=string.Empty;
 
@@ -44,3 +44,29 @@ PrintArray();
 Console.WriteLine("------");
 s=GetString (array);
 Console.WriteLine(s);
+*/
+
+/*Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой 
+все заглавные буквы заменены на строчные. 
+“aBcD1ef!-” => “abcd1ef!-” */
+
+
+Console.Write("Введите строку, содержащую латинские буквы в обоих регистрах: ");
+string s=Console.ReadLine();
+string news=string.Empty;
+
+for(int i=0;i<s.Length;i++)
+{   
+    news += s.ToLower()[i];
+}
+ 
+Console.WriteLine("-------");
+Console.WriteLine(s+" => "+news);
+
+
+/*Задайте произвольную строку. Выясните, является ли она палиндромом. 
+“aBcD1ef!-” => Нет 
+“шалаш” => Да 
+“55655” => Да */
+
+
