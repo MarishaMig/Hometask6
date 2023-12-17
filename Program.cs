a72bb3d28331c6522c8f4e0a0c3fbe68d0a77985
Marina Migurskaya <marina.migurskaya@gmail.com>
2023-12-17 13:20:42 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива. 
+a b c => “abcdef” 
+d e f */
+
+char[,] array = new char[4,3];
+string s=string.Empty;
+
+void CreateArray()
+{
+         for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Convert.ToChar(new Random().Next('a', 'a' + 27));
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+string GetString (char[,] array)
+{
+    string s=string.Empty;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            s+=array[i,j];
+        }
+    }
+    return s;
+}
+CreateArray();
+PrintArray();
+Console.WriteLine("------");
+s=GetString (array);
+Console.WriteLine(s);
